1c9fe5d04b2769ac2928c6058abaa3126ac91f5a
Brian Addicks <brian@addicks.us>
2018-12-14 14:04:09 -0500

    replaced system.web.httputility with system.uri !deploy

--- prtgshell/Classes/Helpers/HelperWeb.Class.ps1
+++ prtgshell/Classes/Helpers/HelperWeb.Class.ps1
@@ -5,7 +5,7 @@ class HelperWeb {
         # Sorting hashtable to make testing for proper URLs reliable.
         foreach ($hash in ($hashTable.GetEnumerator() | Sort-Object -Property Name)) {
             $i++
-            $queryString += $hash.Name + "=" + [System.Web.HttpUtility]::UrlEncode($hash.Value)
+            $queryString += $hash.Name + "=" + [System.Uri]::UrlEncode($hash.Value)
             if ($i -lt $HashTable.Count) {
                 $queryString += "&"
             }
