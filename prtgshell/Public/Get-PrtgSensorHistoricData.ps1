c28e298e41c9c42b7df50bf33910f2c7d0123621
Aubrey Blackburn <aubrey@Aubreys-MacBook-Pro.local>
2019-02-13 14:15:34 -0500

    create initial commit for port for GetHistoicData

---
+++ prtgshell/Public/Get-PrtgSensorHistoricData.ps1
@@ -0,0 +1,37 @@
+Function Get-PrtgSensorHistoricData {
+	[CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [int] $SensorId,
+
+		[Parameter(Mandatory=$True,Position=1)]
+		[datetime] $RangeStart,
+
+		[Parameter(Mandatory=$True,Position=2)]
+		[datetime] $RangeEnd,
+
+		[Parameter(Mandatory=$True,Position=3)]
+		[int] $IntervalInSeconds = 3600
+    )
+
+    BEGIN {
+		$PrtgServerObject = $Global:PrtgServerObject
+    }
+
+    PROCESS {
+		$Parameters = @{
+			$QueryTable.id = $SensorId
+			$QueryTable.sdate = $RangeStart.ToString("yyyy-MM-dd-HH-mm-ss")
+			$QueryTable.edate = $RangeEnd.ToString("yyyy-MM-dd-HH-mm-ss")
+			$QueryTable.avg = $IntervalInSeconds
+		}
+
+				$Response = $global:PrtgServerObject.invokeApiQuery($QueryTable, $QueryPage)
+
+				$DataPoints += $Response.RawData | ConvertFrom-Csv | ? { $_.'Date Time' -ne 'Averages' }
+	}
+
+	END {
+		return $DataPoints
+    }
+}
